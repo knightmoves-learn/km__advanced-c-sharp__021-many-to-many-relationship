a5b23cd4d9c2465623483605f6a5b89c9e7ae611
Isaac <isaac@knightmoves.org>
2025-02-21 16:21:10 -0600

    QA

--- HomeEnergyApi/Dtos/UtilityProviderDto.cs
+++ HomeEnergyApi/Dtos/UtilityProviderDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HomeEnergyApi.Dtos
 {
     public class UtilityProviderDto
     {
+        [Required]
         public string Name { get; set;}
+        [Required]
         public ICollection<string> ProvidedUtilities { get; set;}
     }
 }
