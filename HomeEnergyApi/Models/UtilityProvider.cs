3771fee1ea2b21a1d2a5c6fb993aa0f3c9883c88
Isaac <isaac@knightmoves.org>
2025-02-21 16:19:18 -0600

    QA

--- HomeEnergyApi/Models/UtilityProvider.cs
+++ HomeEnergyApi/Models/UtilityProvider.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HomeEnergyApi.Models
 {
     public class UtilityProvider
     {
         public int Id {get; set;}
+
+        [Required]
         public string Name {get; set;}
+
+        [Required]
         public ICollection<string> ProvidedUtilities {get; set;}
+        
+        [Required]
         public ICollection<HomeUtilityProvider> HomeUtilityProviders {get; set;}
     }
 }
